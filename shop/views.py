faa5dd75879d68706a2854cf2ae8ecd9eb1cd6ac
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2020-09-11 14:34:13 +0100

    Add index view

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from shop.models import Products
+from django.core.paginator import Paginator
+
+
+def index(request):
+    products = Products.objects.all()
+    latest_sale = products.order_by("-price")[:4]
+    item_name = request.GET.get('item_name')
+    if item_name != "" and item_name is not None:
+        products = products.filter(title__contains=item_name)
+    
+    paginator = Paginator(products, 4)
+    page = request.GET.get("page")
+    products = paginator.get_page(page)
+    
+    return render(request, "shop/index.html", {
+        "products":products,
+        "latest_sale":latest_sale
+        })
