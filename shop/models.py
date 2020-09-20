705ad1649b54b128568805c407bb83a2f0b7269b
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2020-09-20 16:10:13 +0100

    Add Order Model

--- shop/models.py
+++ shop/models.py
@@ -7,4 +7,17 @@ class Products(models.Model):
     discount_price = models.FloatField()
     category = models.CharField(max_length=200)
     description = models.TextField()
-    Image = models.CharField(max_length=300)
+    Image = models.CharField(max_length=600, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Order(models.Model):
+    items = models.CharField(max_length=1200)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    address = models.CharField(max_length=500)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
