4090c21d6d4c3eb99ccb370775b64cc3c3934f10
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2020-09-20 16:12:58 +0100

    Add Detail Path

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from shop import views
 urlpatterns = [ 
      path('', views.index, name="home"),
-    
+     path('products', views.shop, name="shop"),
+     path('<int:pk>', views.product_detail, name="detail"),
+     path('checkout', views.checkout, name="checkout"),
 ]
