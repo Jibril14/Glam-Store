db5b2c76dcecf7c07d7f2a6d07718d0319cfac2d
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2020-09-22 16:14:56 +0100

    Add detail views

--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from shop.models import Products
+from shop.models import Products, Order
 from django.core.paginator import Paginator
 
 
@@ -21,3 +21,27 @@ def index(request):
         "products":products,
         "latest_sale":latest_sale
         })
+
+def shop(request):
+    all_products = Products.objects.all()
+    return render(request, "shop/shop.html", {"all_products":all_products})
+
+def product_detail(request, pk):
+    single_product = Products.objects.get(id=pk)
+    return render(request, "shop/detail.html", {"single_product":single_product})
+
+def checkout(request):
+
+    if request.method == "POST":
+        items = request.POST.get("items", "")
+        name = request.POST.get("name", "")
+        email = request.POST.get("email", "")
+        address = request.POST.get("address", "")
+        city = request.POST.get("city", "")
+        state = request.POST.get("state", "")
+        zipcode = request.POST.get("zipcode", "")
+    
+        order = Order(items=items, name=name, email=email, address=address, city=city, state=state, zipcode=zipcode)
+        order.save()
+    return render(request, "shop/checkout.html")
+
