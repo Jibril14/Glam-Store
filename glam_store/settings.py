98de415d3e274e2035881a2977909c3d967a59c0
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2020-11-19 00:28:43 +0100

    Configure Allowed Host

--- glam_store/settings.py
+++ glam_store/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://glamstore.herokuapp.com/", "127.0.0.1"]
 
 
 # Application definition
